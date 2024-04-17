66d0c391ee52dcc391d9418f91d2cc68cc48f047
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-04-17 21:12:46 +0300

    update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,19 @@
+import myVideo from "../../images/first.mp4";
+import styles from "./HomePage.module.css";
+
 function HomePage() {
-  return <div>This is my homepage</div>;
+  return (
+    <section className={styles.videoSection}>
+      <video controls autoPlay src={myVideo} className={styles.videoPlayer}>
+        {myVideo}
+      </video>
+      <h1 className={styles.homePageTitle}>Alex the barber</h1>
+      <p className={styles.mottoParagraph}>
+        "În fiecare tăietură descopăr povestea ta. Frizerul care îți modelează
+        nu doar părul, ci și încrederea în propria ta frumusețe."
+      </p>
+    </section>
+  );
 }
 
 export default HomePage;
